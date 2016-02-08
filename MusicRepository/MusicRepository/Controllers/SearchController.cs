c10cce972e62fb170aa757689078f36254164efa
Brooler <maryaninin97@hotmail.com>
2016-02-08 21:40:17 +0200

    MusicRepository

---
+++ MusicRepository/MusicRepository/Controllers/SearchController.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity.Core.Common.CommandTrees;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MusicRepository.Models;
+
+namespace MusicRepository.Controllers
+{
+    public class SearchController : Controller
+    {
+        MusicContext db = new MusicContext();
+        // GET: Search
+        public PartialViewResult SearchBar()
+        {
+            SearchModel model = new SearchModel();
+            return PartialView(model);
+        }
+        [HttpPost]
+        public ActionResult SearchResult(string query)
+        {
+            return View(SearchResultsInitializer(query));
+        }
+
+
+        //private methods
+        private SearchResults SearchResultsInitializer(string query)
+        {
+            SearchResults results = new SearchResults
+            {
+                Query = query,
+                Autors = AutorInitializer(query),
+                Albums = AlbumInitializer(query),
+                Tracks = TrackInitializer(query)
+            };
+            return results;
+        }
+        private List<Autor> AutorInitializer(string query)
+        {
+            return db.Autors.Where(p => p.Name.Contains(query)).ToList();
+        }
+
+        private List<Album> AlbumInitializer(string query)
+        {
+            return db.Albums.Where(p => p.Name.Contains(query)).ToList();
+        }
+
+        private List<TrackDetail> TrackInitializer(string query)
+        {
+            List<TrackDetail> result = new List<TrackDetail>();
+            foreach (var item in db.Tracks.Where(p=>p.Name.Contains(query)))
+            {
+                result.Add(TrackDetailBuilder(item));
+            }
+            return result;
+        }
+
+        private TrackDetail TrackDetailBuilder(Track track)
+        {
+            TrackDetail result = new TrackDetail();
+            result.Id = track.TrackId;
+            result.AlbumId = track.AlbumId;
+            result.TrackName = track.Name;
+            result.Rate = track.Rate;
+            Album album = db.Albums.Find(track.AlbumId);
+            result.AlbumName = album.Name;
+            result.AutorId = album.AutorId;
+            Autor autor = db.Autors.Find(album.AutorId);
+            result.AutorName = autor.Name;
+            return result;
+        }
+    }
+}
---
+++ MusicRepository/MusicRepository/Models/SearchViewModels.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace MusicRepository.Models
+{
+    public class SearchResults
+    {
+        public string Query { get; set; }
+        public List<Autor> Autors { get; set; }
+        public List<Album> Albums { get; set; }
+        public List<TrackDetail> Tracks { get; set; }  
+    }
+
+    public class SearchModel
+    {
+        [Required]
+        public string Query { get; set; }
+    }
+}
